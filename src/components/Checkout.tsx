ee03da18e056170053d492a2fd4e2c40d853dd99
karabashtawi <suhahijazi9@gmail.com>
2026-09-04 00:57:02 +0300

    Update Checkout.tsx

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -8,7 +8,6 @@ import { COLORS, CITIES } from '../i18n';
 import { Bottle } from './Bottle';
 import { supabase } from '../lib/supabase';
 
-const MAX_STOCK = 
 const UNIT = 15;
 const DELIVERY = 2;
 const SHEETDB_URL = 'https://sheetdb.io/api/v1/0w62f5fizw9co';
